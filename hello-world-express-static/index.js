7d8d4c09c5feee767fd0a2c3b9f923ff80469a5a
winstoncwang <congwang@live.co.uk>
2020-09-12 20:38:44 +0100

    Include app.json and update package.json for deployment

--- hello-world-express-static/index.js
+++ hello-world-express-static/index.js
@@ -8,21 +8,21 @@ const cors = require('cors');
 const path = require('path');
 
 //port setup
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 //serve static resource
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 //express get
-app.get('/', (req, res) => {
-	res.status(200);
-});
-
-const server = app.listen(PORT, () => {
-	console.log(`CORS enabled on port <<${PORT}>> ----`);
-	console.log(`listening on port <<${PORT}>> ----`);
-});
+app
+	.get('/', (req, res) => {
+		res.status(200).render('index');
+	})
+	.listen(PORT, () => {
+		console.log(`CORS enabled on port <<${PORT}>> ----`);
+		console.log(`listening on port <<${PORT}>> ----`);
+	});
 
 //commonJS not ES6 because of nodejs
-module.exports = server;
+//module.exports = server;
