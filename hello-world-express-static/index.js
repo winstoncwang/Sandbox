d75993e8345f8ce7e901e804a30bfb2d803024b4
winstoncwang <congwang@live.co.uk>
2020-09-12 20:44:03 +0100

    deployment try no.2

--- hello-world-express-static/index.js
+++ hello-world-express-static/index.js
@@ -11,18 +11,11 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 //serve static resource
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
-
-//express get
-app
-	.get('/', (req, res) => {
-		res.status(200).render('index');
-	})
-	.listen(PORT, () => {
-		console.log(`CORS enabled on port <<${PORT}>> ----`);
-		console.log(`listening on port <<${PORT}>> ----`);
-	});
+express()
+	.use(express.static(path.join(__dirname, 'public')))
+	.use(cors())
+	.get('/', (req, res) => res.status(200).render('index'))
+	.listen(PORT, () => console.log(`listening on port <<${PORT}>> ----`));
 
 //commonJS not ES6 because of nodejs
 //module.exports = server;
